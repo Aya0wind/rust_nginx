298ce08ab5f238ad03a3fdc074599018749bc6ea
Aya0wind <120035959@qq.com>
2021-08-05 16:31:17 +0800

    dev start

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 #[macro_use]
 extern crate log;
 
-use hyper::{Body, Client, Request, Response, Server};
-use hyper::client::HttpConnector;
-use hyper::Result;
-use hyper::service::{make_service_fn, service_fn};
 use mimalloc::MiMalloc;
-use tokio::time::timeout_at;
+use std::collections::HashMap;
+use mlua::Lua;
+use std::sync::{Arc, Mutex};
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
@@ -14,32 +12,35 @@ static GLOBAL: MiMalloc = MiMalloc;
 
 mod router;
 mod server;
-async fn main_handle(
-    req: Request<Body>,
-) -> Result<Response<Body>> {
-    trace!("Get Request:{:?}", req);
-    router::params(&req);
-    Ok(Response::new(Body::from("Hello")))
-}
 
 
-#[tokio::main]
-async fn main() -> std::result::Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    pretty_env_logger::init();
-    let address=([127,0,0,1],80).into();
-    let server = server::make_server(&address);
-    let interrupt = tokio::signal::ctrl_c();
-    //let handle = tokio::spawn(server1);
-    //let res = handle.await;
-    tokio::spawn(interrupt).await??;
-    eprintln!("Server interrupted by ctrl-c signal\nBye");
-    // tokio::select! {
-    //     v =  =>{
-    //         v??;
-    //     }
-    //     _ = tokio::spawn(interrupt) => {
-    //         eprintln!("Server interrupted by ctrl-c signal\nBye");
-    //     }
-    // };
-    Ok(())
+fn main() -> std::result::Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let rt  = tokio::runtime::Builder::new_multi_thread().worker_threads(16).enable_all().build()?;
+    rt.block_on(async {
+        let mut context=HashMap::<i32,i32>::new();
+
+
+        pretty_env_logger::init();
+        let address0=([127,0,0,1],80).into();
+        let address1=([127,0,0,1],81).into();
+        let server0 = server::make_server(address0);
+        let handle0 = tokio::spawn(server0.await);
+        let server1 = server::make_server(address1);
+        let handle1 = tokio::spawn(server1.await);
+        let mut vec = vec![handle0];
+        vec.push(handle1);
+        let interrupt = tokio::signal::ctrl_c();
+
+        tokio::spawn(interrupt).await??;
+        eprintln!("Server interrupted by ctrl-c signal\nBye");
+        // tokio::select! {
+        //     v =  =>{
+        //         v??;
+        //     }
+        //     _ = tokio::spawn(interrupt) => {
+        //         eprintln!("Server interrupted by ctrl-c signal\nBye");
+        //     }
+        // };
+        Ok(())
+    })
 }
--- src/server.rs
+++ src/server.rs
@@ -7,21 +7,43 @@ use hyper::Server;
 use hyper::Result;
 use hyper::server::conn::{AddrStream, AddrIncoming, Http};
 use std::net::SocketAddr;
+use std::sync::Arc;
+use std::borrow::BorrowMut;
+use mlua::Lua;
 
 
 async fn main_handle(
     req: Request<Body>,
 ) -> hyper::Result<Response<Body>> {
+    let lua = Lua::new();
     trace!("Get Request:{:?}", req);
-    Ok(Response::new(Body::from("Hello")))
+    Ok(Response::new(Body::from(r##""##)))
+}
+
+struct Config;
+
+
+struct ServerContext{
+    config:Arc<Config>
+}
+impl ServerContext{
+    fn update(&mut self){
+        let boxed_config = Box::new(Config{});
+    }
 }
 
 pub async fn make_server(
-    address: &SocketAddr,
-) -> Box<dyn Future<Output=Result<()>>> {
-    let make_service = make_service_fn(|_conn| async {
-        Ok::<_, hyper::Error>(service_fn(main_handle))
+    address: SocketAddr
+) ->impl Future<Output=Result<()>> {
+    let make_service = make_service_fn(|_conn|{
+
+        async { Ok::<_, hyper::Error>(service_fn(move |req|{
+
+            main_handle(req)
+        })) }
     });
     // Then bind and serve...
-    Box::new(Server::bind(&address).serve(make_service))
+    Server::bind(&address).serve(make_service)
 }
+
+
